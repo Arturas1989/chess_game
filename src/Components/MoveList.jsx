505c274373069f3064f100cead0d324c42aa1453
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-10-04 20:32:58 +0300

    #67 components

--- src/Components/MoveList.jsx
+++ src/Components/MoveList.jsx
@@ -1,9 +1,47 @@
 import { useState, useEffect } from 'react';
+import { useGameContext } from '../themes/themes.js';
 
 const MoveList = () => {
+    const { chess } = useGameContext();
     return (
-      <div className="MoveList"></div>
+      <div className="MoveList">
+        <MoveRow chess={chess}/>
+      </div>
     );
+}
+
+const MoveRow = ({ chess }) => {
+  return (
+    <div className="MoveRow">
+      <div className="MoveNumber"></div>
+      <MovesCol chess={chess}/>
+    </div>
+  )
+}
+
+const MovesCol = ({ chess }) => {
+  return (
+    <div className="MovesCol">
+      <WhiteMove chess={chess}/>
+      <BlackMove chess={chess}/>
+    </div>
+  )
+}
+
+const WhiteMove = ({ chess }) => {
+  return (
+    <div className="WhiteMove">
+
+    </div>
+  )
+}
+
+const BlackMove = ({ chess }) => {
+  return (
+    <div className="BlackMove">
+
+    </div>
+  )
 }
 
 export default MoveList;

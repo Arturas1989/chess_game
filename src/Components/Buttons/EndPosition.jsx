e8e5de59d62dea456cb30a549afb0421703256b1
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-11-06 19:14:34 +0200

    #129 Button icons

--- src/Components/Buttons/EndPosition.jsx
+++ src/Components/Buttons/EndPosition.jsx
@@ -4,8 +4,25 @@ import { useGameContext } from '../../GameApp.js';
 const EndPosition = () => {
     return (
         <button className='EndPosition'>
-            a
+            <TrianglesRight />
         </button>
+    )
+}
+
+const TrianglesRight = () => {
+    return (
+        <svg className='MoveControlSvg' width="16" height="16" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g>
+                <g>
+                    <path d="M9.113,15.495 C8.531,16.076 7.01,16.395 7.01,14.494 L7.01,1.506 C7.01,-0.333 8.531,-0.076 9.113,0.506 L15.557,6.948 C16.137,7.529 16.137,8.47 15.557,9.052 L9.113,15.495 L9.113,15.495 Z" >
+        
+                    </path>
+                    <path d="M2.113,15.495 C1.531,16.076 0.01,16.395 0.01,14.494 L0.01,1.506 C0.01,-0.333 1.531,-0.076 2.113,0.506 L8.557,6.948 C9.137,7.529 9.137,8.47 8.557,9.052 L2.113,15.495 L2.113,15.495 Z" >
+        
+                    </path>
+                </g>
+            </g>
+        </svg>
     )
 }
 
--- src/Components/Buttons/PrevMove.jsx
+++ src/Components/Buttons/PrevMove.jsx
@@ -4,8 +4,22 @@ import { useGameContext } from '../../GameApp.js';
 const PrevMove = () => {
     return (
         <button className='PrevMove'>
-            c
+            <TriangleLeft />
         </button>
+    )
+}
+
+const TriangleLeft = () => {
+    return (
+        <svg className='MoveControlSvg' width="16" height="16" viewBox="4 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g>
+                <g>
+                    <path d="M7.446,9.052 C6.866,8.471 6.866,7.53 7.446,6.948 L13.89,0.506 C14.471,-0.076 15.993,-0.333 15.993,1.506 L15.993,14.494 C15.993,16.395 14.472,16.076 13.89,15.495 L7.446,9.052 L7.446,9.052 Z">
+
+                    </path>
+                </g>
+            </g>
+        </svg>
     )
 }
 

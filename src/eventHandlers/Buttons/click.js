2301fa8fa09ca67fc789b19999e4b3c057a1afde
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-11-07 21:34:18 +0200

    #129 Button onClick event handling logic

---
+++ src/eventHandlers/Buttons/click.js
@@ -0,0 +1,78 @@
+import { setFromToStyles } from '../../utilities/utilities.js';
+
+const goBackToStart =  (gameContext) => {
+    const { setCurrVariant, setStyles, initialStyles } = gameContext;
+    setCurrVariant({'currLine' : 'line1', 'currMove' : 0});
+    setStyles(initialStyles);
+}
+
+const goToNextMove = (gameContext) => {
+    const { 
+        chessVariants,
+        currVariant, 
+        setCurrVariant, 
+        setStyles, 
+        initialStyles, 
+        preComputedMaps,
+        theme 
+    } = gameContext;
+    
+    const {currLine, currMove} = currVariant;
+    const nextMove = currMove + 1;
+    if(chessVariants[currLine]['moves'][nextMove]){
+        const moves = chessVariants['line1']['moves'];
+        setFromToStyles(moves, nextMove, preComputedMaps, initialStyles, theme, setStyles);
+        setCurrVariant({...currVariant, currMove: nextMove});
+    }
+}
+
+const goToPrevMove = (gameContext) => {
+    const { 
+        chessVariants,
+        currVariant, 
+        setCurrVariant, 
+        setStyles, 
+        initialStyles, 
+        preComputedMaps,
+        theme 
+    } = gameContext;
+
+    let {currLine, currMove} = currVariant;
+    currMove--;
+    if(currMove > -1){
+        const {fromMove, fromLine} = chessVariants[currLine];
+        if(fromMove === currMove) currLine = fromLine;
+        const moves = chessVariants[currLine]['moves'];
+        setFromToStyles(moves, currMove, preComputedMaps, initialStyles, theme, setStyles);
+        setCurrVariant({currLine: currLine, currMove: currMove});
+    }
+}
+
+const goToEnd = (gameContext) => {
+    const { 
+        chessVariants, 
+        setCurrVariant, 
+        setStyles, 
+        initialStyles, 
+        preComputedMaps,
+        theme 
+    } = gameContext;
+
+    const moves = chessVariants['line1']['moves'];
+    const lastIndex = moves.length - 1;
+    setFromToStyles(moves, lastIndex, preComputedMaps, initialStyles, theme, setStyles);
+    setCurrVariant({'currLine' : 'line1', 'currMove' : lastIndex});
+}
+
+const reverseBoard = (gameContext) => {
+    const { setIsReversed } = gameContext;
+    setIsReversed((prev) => !prev);
+}
+
+export { 
+    goBackToStart,
+    goToNextMove,
+    goToPrevMove,
+    goToEnd,
+    reverseBoard
+}

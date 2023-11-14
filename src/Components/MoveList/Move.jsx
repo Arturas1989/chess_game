c36976d84a1df228db805acd75336810dd0d6866
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-11-14 19:23:24 +0200

    removed commented code

--- src/Components/MoveList/Move.jsx
+++ src/Components/MoveList/Move.jsx
@@ -47,16 +47,7 @@ const VariantMove = ({ notation, i, id, dots }) => {
         id={id} 
         onClick={(e) => goToMove(e)}
       >
-        
-          {/* {i % 2 === 0 ? 
-            <MoveNumber 
-              className="VariantMoveNumber" 
-              i={i} 
-              fontSizeType={'variantsFontSize'}
-              dots={dots} 
-            /> 
-          : ''} */}
-          <Notation notation={notation} i={i} dots={dots} fontSizeType={'variantsFontSize'}/>
+        <Notation notation={notation} i={i} dots={dots}/>
       </div>
     )
   }

57fc10ca5baaa15f288429f55f74c8e3ea3dd655
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-11-20 22:08:30 +0200

    #143 search functionality

--- src/Components/Search/SearchBar.jsx
+++ src/Components/Search/SearchBar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import SearchButton from './SearchButton.jsx'
 
-const SearchBar = () => {
+
+const SearchBar = ( { searchVals, onSearchValsChange }) => {
+    const {username} = searchVals
     return (
         <div className='SearchBar'>
-            <input  placeholder='search by nickname'></input>
-            <SearchButton />
+            <input  
+                placeholder='search by nickname'
+                value={username} 
+                onChange={(e) => onSearchValsChange({...searchVals, username : e.target.value})} 
+            />
+            <SearchButton searchVals={searchVals}/>
         </div>
     )
 }

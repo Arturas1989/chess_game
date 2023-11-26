c604ad9876ba36651ad1102197faa20d23d8e9da
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-11-26 20:32:18 +0200

    #143 search on key "Enter"

--- src/Components/Search/SearchBar.jsx
+++ src/Components/Search/SearchBar.jsx
@@ -3,20 +3,29 @@ import { useGameContext } from '../../GameApp.js';
 import { searchChessGames } from '../../utilities/api.js';
 
 const SearchBar = () => {
-    const {setApiData, API_URL, searchVals, setSearchVals, setCurrView} = useGameContext();
-    const {username} = searchVals
+    const {
+        setApiData, 
+        API_URL, 
+        searchVals, 
+        setSearchVals, 
+        setCurrView, 
+        setErrors
+    } = useGameContext();
 
-    const handleSearchButtonClick = () => {
-        searchChessGames(API_URL, searchVals, setApiData);
-        if(searchVals.username) setCurrView('games');
+    const {username} = searchVals;
+
+    const handleSearchButtonClick = async () => {
+        const data = await searchChessGames(API_URL, searchVals, setApiData, setErrors);
+        if(searchVals.username && data.length !== 0) setCurrView('games');
     }
 
-    return (
+    return (  
         <div className='SearchBar'>
             <input  
                 placeholder='search by chess.com username'
                 value={username} 
-                onChange={(e) => setSearchVals({username : e.target.value, title: ''})} 
+                onChange={(e) => setSearchVals({username : e.target.value, title: ''})}
+                onKeyDown={(e) => e.key === 'Enter' ? handleSearchButtonClick() : null}
             />
             <SearchButton handleClick={handleSearchButtonClick} />
         </div>

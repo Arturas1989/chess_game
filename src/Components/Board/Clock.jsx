ab823e9a934e8be7c3ac5924056a7716789111a0
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-12-01 22:45:25 +0200

    moved state control to parent

--- src/Components/Board/Clock.jsx
+++ src/Components/Board/Clock.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from 'react';
 import { useGameContext } from "../../GameApp";
 
-const Clock = ({ color }) => {
-    const { playControls, chessVariants } = useGameContext();
-    const [sec, setSec] = useState(playControls[`${color}MainTime`]);
 
+const Clock = ({ color, clock, setClock }) => {
+    const { playControls, setPlayControls, chessVariants } = useGameContext();
+    
+    const sec = clock[color];
     const m = Math.floor(sec / 60).toString().padStart(2,'0');
     const s = (sec % 60).toString().padStart(2,'0');
     const turn = chessVariants.line1.moves.length % 2 === 1 ? 'white' : 'black';
     
+
+    
     useEffect(() => {
         if(turn === color){
             const interval = setInterval(() => {
-                setSec(sec - 1)
+                setClock({...clock, [color] : sec - 1});
             }, 1000)
     
             return () => clearInterval(interval);

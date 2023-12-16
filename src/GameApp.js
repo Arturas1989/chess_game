3c3070f54c4e14dd7d2ee8e5491fcf9a0354ed1a
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-12-16 16:04:39 +0200

    formatting and restructured GameApp

--- src/GameApp.js
+++ src/GameApp.js
@@ -1,168 +1,112 @@
 import './App.css';
-import { useState, createContext, useContext } from 'react';
-import themes from './themes/themes.js';
+import { useState } from 'react';
 import { Board, PromotionBoard } from './Components/Board/Board.jsx';
 import Player from './Components/Board/Player.jsx';
+import PlayerContainer from './Components/Board/PlayerContainer.jsx';
 import Result from './Components/Board/Result.jsx';
 import MoveContainer from './Components/MoveList/MoveContainer.jsx';
 import SearchContainer from './Components/Search/SearchContainer.jsx';
-import { preComputed, setInitialStyles, promotionPieces } from './utilities/utilities.js';
-import { Chess } from 'chess.js';
 import SearchResults from './Components/Search/SearchResults.jsx';
 import PlayModal from './Components/Modal/PlayModal.jsx';
 import Clock from './Components/Board/Clock.jsx';
-import useChessEngine from './hooks/useChessEngine.jsx';
-
-
-const GameContext = createContext();
-const API_URL = 'https://api.chess.com/pub/player/'
+import {
+    GameContextProvider,
+    useGameContext,
+} from './context/GameContextProvider.jsx';
 
 const GameApp = () => {
-  const [theme, setTheme] = useState({squares: 'standard', pieces: 'standard', background: 'light'});
-  const [promotion, setPromotion] = useState({isPromoting: false, from: '', to: '', pieceType: ''});
-  const [chessVariants, setChessVariants] = useState({
-    'line1' : {
-      'moves' : [new Chess()],
-      'fromMove' : 0,
-      'fromLine' : 'line1'
-    },
-    'lastLine' : 1,
-    'movesLines' : {}
-  });
-  const [currVariant, setCurrVariant] = useState({'currLine' : 'line1', 'currMove' : 0});
-  const [isReversed, setIsReversed] = useState(false);
-  const [pieceClicked, setPieceClicked] = useState({});
-  const [boardBoundaries, setBoardBoundaries] = useState(null);
-  const [apiData, setApiData] = useState([]);
-  const [searchVals, setSearchVals] = useState({username : '', title : ''});
-  const [currView, setCurrView] = useState('board');
-  const [partname, setPartname] = useState('');
-  const [errors, setErrors] = useState({userSearchError: ''});
-  const [apiGame, setApiGame] = useState({});
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [playControls, setPlayControls] = useState({isPlaying: false, result: ''});
-  
-  const { coords, revCoords, coordToId, idToCoord, revCoordToId, revIdToCoord } = preComputed;
+    return (
+        <GameContextProvider>
+            <GameContainer />
+        </GameContextProvider>
+    );
+};
 
-  let initialStyles = {};
-  setInitialStyles(coords, initialStyles, theme.squares + 'Square');
-  
-    
-  let preComputedMaps, promotionPiecesList;
+const GameContainer = () => {
+    const {
+        apiData,
+        currView,
+        isReversed,
+        apiGame,
+        modalIsOpen,
+        playControls,
+        promotion,
+    } = useGameContext();
+    const isPromoting = promotion.isPromoting;
 
-  if(isReversed){
-    preComputedMaps = [revCoords, revCoordToId, revIdToCoord];
-    promotionPiecesList = promotionPieces.reversed;
-  } else {
-    preComputedMaps = [coords, coordToId, idToCoord];
-    promotionPiecesList = promotionPieces.regular;
-  }
+    const [clock, setClock] = useState({ white: 0, black: 0 });
 
-  const [styles, setStyles] = useState({...initialStyles});
+    let ClockComponent1,
+        ClockComponent2,
+        players = { white: '', black: '' },
+        displayPlayerContainer = false;
+    if (playControls.isPlaying) {
+        ClockComponent1 = (
+            <Clock color="white" clock={clock} setClock={setClock} />
+        );
+        ClockComponent2 = (
+            <Clock color="black" clock={clock} setClock={setClock} />
+        );
+        players =
+            playControls.color === 'white'
+                ? { white: 'you', black: 'chess engine' }
+                : { white: 'chess engine', black: 'you' };
 
-  
-  useChessEngine(playControls, chessVariants, setChessVariants, setPlayControls, setCurrVariant, setIsReversed);
-  
+        if (isReversed)
+            [ClockComponent1, ClockComponent2] = [
+                ClockComponent2,
+                ClockComponent1,
+            ];
+        displayPlayerContainer = true;
+    } else if (Object.keys(apiGame).length) {
+        const { whiteUsername, blackUsername } = apiGame;
+        players = { white: whiteUsername, black: blackUsername };
+        displayPlayerContainer = true;
+    }
 
-  const GameContextValues = {
-    themes,
-    theme,
-    setTheme,
-    promotion, 
-    setPromotion,
-    currVariant,
-    setCurrVariant,
-    chessVariants,
-    setChessVariants,
-    isReversed, 
-    setIsReversed,
-    preComputedMaps,
-    promotionPiecesList,
-    styles,
-    setStyles,
-    initialStyles,
-    pieceClicked,
-    setPieceClicked,
-    boardBoundaries, 
-    setBoardBoundaries,
-    apiData, 
-    setApiData,
-    searchVals, 
-    setSearchVals,
-    API_URL,
-    currView, 
-    setCurrView,
-    partname, 
-    setPartname,
-    errors, 
-    setErrors,
-    apiGame, 
-    setApiGame,
-    modalIsOpen, 
-    setModalIsOpen,
-    playControls, 
-    setPlayControls
-  }
+    return currView !== 'board' ? (
+        <SearchResults data={apiData} />
+    ) : (
+        <div className="GameContainer">
+            <div className="GameWrap">
+                {modalIsOpen ? <PlayModal setClock={setClock} /> : ''}
+                <SearchContainer />
 
-  return (
-    <GameContext.Provider value={GameContextValues}>
-      <GameContainer isPromoting={promotion.isPromoting} />
-    </GameContext.Provider>
-  )
-}
+                <div className="GameControls">
+                    <div className="BoardContainer">
+                        <Result resultText={playControls.result} />
+                        <PlayerContainer
+                            className="Board-top"
+                            displayPlayerContainer={displayPlayerContainer}
+                        >
+                            <Player
+                                type="top"
+                                players={players}
+                                isReversed={isReversed}
+                            />
+                            {ClockComponent2}
+                        </PlayerContainer>
 
-const GameContainer =  ({ isPromoting }) => {
-  const {apiData, currView, isReversed, apiGame, modalIsOpen, playControls} = useGameContext();
+                        {isPromoting ? <PromotionBoard /> : <Board />}
 
-  const [clock, setClock] = useState({'white' : 0, 'black' : 0});
+                        <PlayerContainer
+                            className="Board-bottom"
+                            displayPlayerContainer={displayPlayerContainer}
+                        >
+                            <Player
+                                type="bottom"
+                                players={players}
+                                isReversed={isReversed}
+                            />
+                            {ClockComponent1}
+                        </PlayerContainer>
+                    </div>
 
-  let ClockComponent1, ClockComponent2, players = {'white' : '', 'black' : ''};
-  if(playControls.isPlaying){
-    ClockComponent1 = <Clock color="white" clock={clock} setClock={setClock} />;
-    ClockComponent2 = <Clock color="black" clock={clock} setClock={setClock} />;
-    players = playControls.color === 'white' ? 
-    {'white' : 'you', 'black' : 'chess engine'} : {'white' : 'chess engine', 'black' : 'you'}; 
-    
-    if(isReversed) [ClockComponent1, ClockComponent2] = [ClockComponent2, ClockComponent1];
-  } else if(Object.keys(apiGame).length){
-    const {whiteUsername, blackUsername} = apiGame;
-    players = {'white' : whiteUsername, 'black' : blackUsername}
-  }
-
-  return (
-    
-    currView !== 'board' ?
-      <SearchResults data={apiData}/>
-      :
-      <div className="GameContainer">
-        {modalIsOpen ? <PlayModal setClock={setClock}/> : ''}
-        <SearchContainer />
-        <div className="GameControls">
-          <div className="BoardContainer">
-            <div className="Board-top">
-              <Player type="top" players={players} isReversed={isReversed} />
-              <Result resultText={playControls.result}/>
-              {ClockComponent2}
+                    <MoveContainer />
+                </div>
             </div>
-            {isPromoting ?
-              <PromotionBoard/>
-              :
-              <Board/>
-            }
-            <div className="Board-bottom">
-              <Player type="bottom" players={players} isReversed={isReversed} />
-              {ClockComponent1}
-            </div>
-          </div>
-          <MoveContainer />
         </div>
-      </div>
-  );
-}
+    );
+};
 
-function useGameContext(){
-    const context = useContext(GameContext);
-    return context;
-}
-
-export {GameApp, useGameContext};
+export { GameApp };

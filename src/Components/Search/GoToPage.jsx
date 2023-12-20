214fdba241b1875ef67dc6d9b6aca82e943cb792
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2023-12-20 18:42:32 +0200

    #162 winner bug fix

--- src/Components/Search/GoToPage.jsx
+++ src/Components/Search/GoToPage.jsx
@@ -1,11 +1,12 @@
 import { useGameContext } from '../../context/GameContextProvider.jsx';
 
 const GoToPage = ({ page, innerText }) => {
-    const { setCurrView, setApiData } = useGameContext();
+    const { setCurrView, setApiData, setPlayControls } = useGameContext();
     
     const handleClick = () => {
         setCurrView(page);
         setApiData([]);
+        setPlayControls({isPlaying: false, result: ''});
     }
 
     return (
